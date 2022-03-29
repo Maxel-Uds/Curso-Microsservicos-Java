4084167b17cea83cba348ef89797bad429d30644
Maxel-Uds <maxellopes32@gmail.com>
2022-03-29 10:57:50 -0300

    Projeto hr-worker criado

---
+++ hr-worker/src/main/java/com/maxeluds/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.maxeluds.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
