71fe9bba157f7b9a8cd021da1e3e2e47c67fb61e
Maxel-Uds <maxellopes32@gmail.com>
2022-03-29 16:31:18 -0300

    Comunicação entre projetos com Feign Client

--- hr-payroll/src/main/java/com/maxeluds/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/maxeluds/hrpayroll/services/PaymentService.java
@@ -2,22 +2,15 @@ package com.maxeluds.hrpayroll.services;
 
 import com.maxeluds.hrpayroll.entities.Payment;
 import com.maxeluds.hrpayroll.entities.Worker;
+import com.maxeluds.hrpayroll.feignClients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
     @Autowired
-    private RestTemplate restTemplate;
-
-    @Value("${hr-worker.host}")
-    private String workerHost;
+    private WorkerFeignClient client;
 
     public Payment getPayment(Long workerId, Integer days) {
         var worker = getWorker(workerId);
@@ -25,9 +18,6 @@ public class PaymentService {
     }
 
     private Worker getWorker(Long id) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(id));
-
-        return restTemplate.getForObject(workerHost.concat("/workers/{id}"), Worker.class, uriVariables);
+        return client.findById(id).getBody();
     }
 }
