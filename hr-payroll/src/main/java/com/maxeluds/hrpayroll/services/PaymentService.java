adb5b7dc005a1deb4cb34cdd3035939a46a0a6ca
Maxel-Uds <maxellopes32@gmail.com>
2022-03-29 15:52:05 -0300

    Comunicação entre projetos com RestTemplates

--- hr-payroll/src/main/java/com/maxeluds/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/maxeluds/hrpayroll/services/PaymentService.java
@@ -1,12 +1,33 @@
 package com.maxeluds.hrpayroll.services;
 
 import com.maxeluds.hrpayroll.entities.Payment;
+import com.maxeluds.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
     public Payment getPayment(Long workerId, Integer days) {
-        return new Payment("João", 42.78, days);
+        var worker = getWorker(workerId);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+    }
+
+    private Worker getWorker(Long id) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(id));
+
+        return restTemplate.getForObject(workerHost.concat("/workers/{id}"), Worker.class, uriVariables);
     }
 }
