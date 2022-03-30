58482bde59744d7cc41e868954cb0e487c6c9832
Maxel-Uds <maxellopes32@gmail.com>
2022-03-30 20:35:21 -0300

    Hystrix para tolerância a falhas

--- hr-payroll/src/main/java/com/maxeluds/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/maxeluds/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.maxeluds.hrpayroll.resources;
 
 import com.maxeluds.hrpayroll.entities.Payment;
 import com.maxeluds.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +17,13 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         return ResponseEntity.ok(service.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        return ResponseEntity.ok(new Payment("Nome", 400.0, days));
     }
 }
--- hr-worker/src/main/java/com/maxeluds/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/maxeluds/hrworker/resources/WorkerResource.java
@@ -2,10 +2,7 @@ package com.maxeluds.hrworker.resources;
 
 import com.maxeluds.hrworker.entities.Worker;
 import com.maxeluds.hrworker.repositories.WorkerRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
